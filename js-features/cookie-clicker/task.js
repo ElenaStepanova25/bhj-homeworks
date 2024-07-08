6b108b5802288fd696576e5a29fe666aa04d561f
ElenaStepanova <Elena_stepanova25@mail.ru>
2024-07-08 11:07:34 +0700

    Второе задание

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,1 +1,27 @@
-"use strict"
+//получаем ссылки
+const cookieWidth = document.getElementById("cookie");
+const cookiCounter = document.getElementById("clicker__counter");
+const cookiSpeed = document.getElementById("clicker__speed");
+
+//функция при клике на печенке
+function clickCookie() {
+    // счетчик кликов увеличиваем на 1
+    cookiCounter.textContent++;
+
+    //изменение ширины
+    if (cookiCounter.textContent % 2 === 0) {
+        cookieWidth.width -= 20;
+      } else {
+        cookieWidth.width += 20;
+      }
+
+    //создаем объект дата
+    let date = new Date();
+    //получаем секунды
+    let seconds = date.getSeconds();
+    //рассчитываем скорость клика
+    let speed = Number(cookiCounter.textContent) / seconds;
+    cookiSpeed.textContent = speed;
+}
+
+cookieWidth.onclick = clickCookie;
